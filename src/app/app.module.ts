ec93a58cd582dee19ff8a43e5ea12c687ab9aff5
Admin <smosdevelop@gmail.com>
2023-07-24 18:52:54 +0300

    Шаблон страницы регистрации

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { RegistrationPageComponent } from './components/registration-page/registration-page.component';
+import {MatInputModule} from "@angular/material/input";
+import {FBService} from "./services/fb.service";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    RegistrationPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule
   ],
-  providers: [],
+  providers: [FBService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
